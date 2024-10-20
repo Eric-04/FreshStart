28905bd9abedc50d2d96bc38bda114bac01a4015
baileysmoko <124398895+baileysmoko@users.noreply.github.com>
2024-10-19 21:20:34 -0400

    Revert "feat: check level"
    This reverts commit 2d6590c46221bae8ba46690c156d50861632b0a7.

--- frontend-freshstart/src/firebase.js
+++ frontend-freshstart/src/firebase.js
@@ -1,7 +1,6 @@
 // src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'; // Import Firestore functions
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,8 +11,8 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID
 };
 
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app); // Initialize Firestore
 
-export { auth, db }; // Export Firestore
+export { auth };
